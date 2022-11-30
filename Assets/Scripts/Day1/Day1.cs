df3ecfe88a893be735f4973fdf15b358e82282ca
GodVenn <tlne@equinor.com>
2022-11-30 16:57:49 +0100

    Create placeholder parse and inputs. Ready for AoC 2022

--- Assets/Scripts/Day1/Day1.cs
+++ Assets/Scripts/Day1/Day1.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class Day1 : MonoBehaviour
@@ -7,12 +8,20 @@ public class Day1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        ParseInput();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void ParseInput()
+    {
+        string day = "Day1";
+        string path = $"Assets/Scripts/{day}/input.txt";
+        string content = File.ReadAllText(path);
+        Debug.Log(content);
     }
 }
